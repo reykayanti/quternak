3e9b80a8054dc3705399db2be7592b8076d53a94
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-06-06 02:40:56 +0700

    Update register.php

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/login.css" />
-
-    <title>register</title>
-  </head>
-  <body>
+<?= $this->extend('layout/layout-login'); ?>
+<?= $this->section('content'); ?>
     <div class="register">
       <br />
       <div class="card-log card-log2">
@@ -65,3 +56,4 @@
     </div>
   </body>
 </html>
+<?= $this->endSection(); ?>
